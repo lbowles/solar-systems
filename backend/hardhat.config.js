a7b4ae462b48ed30a2fb130886c2ce3e27b4b6ac
lbowles <lspbowles@gmail.com>
2022-12-22 15:50:15 +0200

    Deploy env

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,16 +1,17 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers")
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-ethers')
 require('hardhat-deploy')
+require('@nomiclabs/hardhat-etherscan')
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,18 +19,30 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require('dotenv').config()
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const MNEMONIC = process.env.MNEMONIC
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
-  defaultNetwork : "hardhat",
-  networks:{
+  defaultNetwork: 'hardhat',
+  networks: {
     hardhat: {},
-    // rinkeby:{
-    //   accounts: {}
-    // }
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
+      saveDeployments: true,
+    },
   },
-  solidity: "0.8.4",
-  namedAccounts : {
-    deployer : {
-      default : 0 
-    }
-  }
-};
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  solidity: '0.8.4',
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+}
