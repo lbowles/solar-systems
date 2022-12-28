bbc5654c62d32bb133f2410a2a44e0636701f171
lbowles <lspbowles@gmail.com>
2022-12-28 21:17:29 +0200

    about dropdowns

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -4,6 +4,9 @@ import style from "./LandingPage.module.css"
 import deployments from "../../src/deployments.json"
 import background from ".././img/background.svg"
 import loading from ".././img/loading.svg"
+import opensea from ".././img/opensea.svg"
+import github from ".././img/github.svg"
+import etherscan from ".././img/etherscan.svg"
 import { ConnectButton, useAddRecentTransaction } from "@rainbow-me/rainbowkit"
 import { useEffect, useState } from "react"
 import { prepareWriteContract, writeContract } from "@wagmi/core"
@@ -17,6 +20,8 @@ import {
 } from "wagmi"
 import { BigNumber } from "ethers"
 import { formatEther } from "ethers/lib/utils.js"
+import { Disclosure, Transition } from "@headlessui/react"
+import { ChevronUpIcon } from "@heroicons/react/20/solid"
 
 const solarSystemsConfig = {
   address: deployments.contracts.SolarSystems.address,
@@ -32,29 +37,17 @@ export function LandingPage() {
 
   const addRecentTransaction = useAddRecentTransaction()
 
-  const {
-    data: mintPrice,
-    isError: isMintPriceError,
-    isLoading: isMintPriceLoading,
-  } = useContractRead({
+  const { data: mintPrice, isError: isMintPriceError, isLoading: isMintPriceLoading } = useContractRead({
     ...solarSystemsConfig,
     functionName: "price",
   })
 
-  const {
-    data: maxSupply,
-    isError: isMaxSupplyError,
-    isLoading: isMaxSupplyLoading,
-  } = useContractRead({
+  const { data: maxSupply, isError: isMaxSupplyError, isLoading: isMaxSupplyLoading } = useContractRead({
     ...solarSystemsConfig,
     functionName: "maxSupply",
   })
 
-  const {
-    data: totalSupply,
-    isError: isTotalSupplyError,
-    isLoading: isTotalSupplyLoading,
-  } = useContractRead({
+  const { data: totalSupply, isError: isTotalSupplyError, isLoading: isTotalSupplyLoading } = useContractRead({
     ...solarSystemsConfig,
     functionName: "totalSupply",
     watch: true,
@@ -75,14 +68,19 @@ export function LandingPage() {
     isSuccess: isMintSignSuccess,
   } = useContractWrite(mintConfig)
 
-  const {
-    data: mintTx,
-    isError: isMintTxError,
-    isLoading: isMintTxLoading,
-  } = useWaitForTransaction({
+  const { data: mintTx, isError: isMintTxError, isLoading: isMintTxLoading } = useWaitForTransaction({
     hash: mintSignResult?.hash,
     confirmations: 1,
   })
+
+  const copy = [
+    {
+      heading: "What are Solar Systems?",
+      body:
+        "Synthetic CryptoPunks is inspired by the historical collection of 10,000 CryptoPunks by Larva Labs and Synthetic Loot by dhof. It generates a unique, fully on-chain CryptoPunk for each Ethereum address.",
+    },
+    { heading: "Features", body: "Each Synthetic Punk Is generated from assets stored fully on-chain" },
+  ]
 
   useEffect(() => {
     const svg = new Blob([getSVG(200)], { type: "image/svg+xml" })
@@ -201,38 +199,44 @@ export function LandingPage() {
         <p className="font-bold">Welcome to the Solar System NFT landing page!</p>
       </div>
       <div className="flex justify-center alignw-screen mt-10 z-1 pl-10 pr-10 relative">
-        <div className=" block p-6 bg-white border border-gray-100 rounded-lg shadow-md ">
-          <div
-            className="grid  md:gap-12 gap-4 text-sm text-justify md:grid-cols-3 text-slate-700"
-            style={{ maxWidth: "800px" }}
-          >
-            <div className="">
-              <p className="font-bold mb-3 text-slate-800">What are they?</p>
-              <p>
-                Our NFTs feature fully on-chain animated SVG solar systems, which means that the media is stored
-                directly on the blockchain and there are no off-chain dependencies. This ensures that your NFTs are
-                secure and fully owned by you.
-              </p>
-            </div>
-            <div className="">
-              <p className="font-bold mb-3 text-slate-800">What are they?</p>
-              <p>
-                Every solar system in our NFTs is unique, as they are generated randomly with a variety of different
-                configurations. No two solar systems are exactly the same, which means that your NFT will truly be
-                one-of-a-kind.
-              </p>
-            </div>
-            <div className="">
-              <p className="font-bold mb-3 text-slate-800">What are they?</p>
-
-              <p>
-                We hope you enjoy collecting and displaying your Solar System NFTs! Start minting your own today and add
-                some cosmic beauty to your collection.
-              </p>
+        <div className="block bg-white border border-gray-100 rounded-lg shadow-md w-[450px]">
+          <div className="w-100 p-5">
+            {copy.map((items, index) => {
+              return (
+                <Disclosure as="div" className={index === 1 ? "mt-3" : ""}>
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-100 px-4 py-2 text-left text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                        <span>{items.heading}</span>
+                        <ChevronUpIcon
+                          className={`${open ? "rotate-360 transform" : "rotate-180"} h-5 w-5 text-gray-800`}
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">{items.body}</Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+              )
+            })}
+          </div>
+          <div className="w-100 bg-slate-900 h-12 -ml-0 -mr-0 translate-y-[1px] rounded-bl-lg rounded-br-lg pt-3">
+            <div className="flex justify-center items-center ">
+              <div className=" grid  grid-flow-col gap-3">
+                <a className="hover:scale-110 duration-100 ease-in-out">
+                  <img src={opensea} alt="opensea" />
+                </a>
+                <a className="hover:scale-110 duration-100 ease-in-out">
+                  <img src={etherscan} alt="etherscan" />
+                </a>
+                <a className="hover:scale-110 duration-100 ease-in-out">
+                  <img src={github} alt="github" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       <div className="flex justify-center alignw-screen mt-24 z-1 pl-10 pr-10 z-10 relative">
         <footer className="sticky w-full py-4  bottom-0 text-center text-gray-700 text-sm">
           Made by{" "}
